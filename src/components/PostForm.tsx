b94dc24373b6fcad2ff587682ffdb204de924f2c
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-10 18:34:42 +0900

    Add authorId functionality

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -5,31 +5,37 @@ type Props = {
   title?: string;
   subTitle?: string;
   content?: string;
+  authorId: string;
   buttonText?: string;
   formAction: (payload: FormData) => void;
-  formState: FormStateType;
+  formState: FormStateType | null;
 };
 
 const PostForm = ({
   title,
   subTitle,
   content,
+  authorId,
   buttonText,
   formAction,
   formState,
 }: Props) => {
   return (
-    <form action={formAction} className='flex flex-col gap-4'>
+    <form
+      action={formAction}
+      className='flex flex-col gap-4 px-8 max-w-3xl mx-auto'
+    >
+      <input type='text' name='authorId' value={authorId} hidden />
       <div className='flex flex-col'>
         <label htmlFor='title' className='ml-2 text-xs'>
           제목:
         </label>
         <input
           type='text'
-          placeholder='아이디'
+          placeholder='제목'
           id='title'
           name='title'
-          value={title || ""}
+          value={title}
           className='border border-black rounded-md p-4'
         />
       </div>
@@ -39,10 +45,10 @@ const PostForm = ({
         </label>
         <input
           type='text'
-          placeholder='아이디'
+          placeholder='부제'
           id='subTitle'
           name='subTitle'
-          value={subTitle || ""}
+          value={subTitle}
           className='border border-black rounded-md p-4'
         />
       </div>
@@ -50,14 +56,13 @@ const PostForm = ({
         <label htmlFor='content' className='ml-2 text-xs'>
           포스팅 내용:
         </label>
-        <input
-          type='text'
-          placeholder='아이디'
+        <textarea
+          placeholder='내용'
           id='content'
           name='content'
-          value={content || ""}
-          className='border border-black rounded-md p-4'
-        />
+          value={content}
+          className='border border-black rounded-md p-4 resize-y h-44'
+        ></textarea>
       </div>
       <FormStatusButton buttonText={buttonText} />
       <FormStateError formState={formState} />
