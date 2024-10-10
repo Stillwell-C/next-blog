a03f50e43ed1300d86a1e7fe58f8511a93e9c82a
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-10 18:32:52 +0900

    Update form size

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -10,47 +10,49 @@ const LoginForm = () => {
   const [state, formAction] = useFormState(credentialsLogin, null);
 
   return (
-    <section className='p-5 flex flex-col border gap-4 border-solid border-black rounded-md'>
-      <h2 className='text-center text-2xl bold'>로그인</h2>
-      <form action={formAction} className='flex flex-col gap-4'>
-        <div className='flex flex-col'>
-          <label htmlFor='username' className='ml-2 text-xs'>
-            아이디:
-          </label>
-          <input
-            type='text'
-            placeholder='아이디'
-            id='username'
-            name='username'
-            className='border border-black rounded-md p-4'
-          />
-        </div>
-        <div className='flex flex-col'>
-          <label htmlFor='password' className='ml-2 text-xs'>
-            비밀번호:
-          </label>
-          <input
-            type='password'
-            placeholder='비밀번호'
-            id='password'
-            name='password'
-            className='border border-black rounded-md p-4'
-          />
-        </div>
-        <FormStatusButton buttonText='로그인' />
-        <FormStateError formState={state} />
-      </form>
-      <Link
-        href='/register'
-        className='border border-black rounded text-center p-3 w-full'
-      >
-        회원가입
-      </Link>
-      <form action={handleGitHubLogin}>
-        <button className='bg-black text-white w-full p-3 rounded'>
-          GitHub 로그인
-        </button>
-      </form>
+    <section className='p-8'>
+      <div className='mx-auto p-6 max-w-xl flex flex-col border gap-4 border-solid border-black rounded-md'>
+        <h2 className='text-center text-2xl bold'>로그인</h2>
+        <form action={formAction} className='flex flex-col gap-4'>
+          <div className='flex flex-col'>
+            <label htmlFor='username' className='ml-2 text-xs'>
+              아이디:
+            </label>
+            <input
+              type='text'
+              placeholder='아이디'
+              id='username'
+              name='username'
+              className='border border-black rounded-md p-4'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <label htmlFor='password' className='ml-2 text-xs'>
+              비밀번호:
+            </label>
+            <input
+              type='password'
+              placeholder='비밀번호'
+              id='password'
+              name='password'
+              className='border border-black rounded-md p-4'
+            />
+          </div>
+          <FormStatusButton buttonText='로그인' />
+          <FormStateError formState={state} />
+        </form>
+        <Link
+          href='/register'
+          className='border border-black rounded text-center p-3 w-full'
+        >
+          회원가입
+        </Link>
+        <form action={handleGitHubLogin}>
+          <button className='bg-black text-white w-full p-3 rounded'>
+            GitHub 로그인
+          </button>
+        </form>
+      </div>
     </section>
   );
 };
