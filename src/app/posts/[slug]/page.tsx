f028bbdfb53ca937d16d396c0d3af3e0bb3bab2c
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-11 15:30:47 +0900

    Add comments & edit button

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,9 @@
+import LinkButton from "@/components/LinkButton";
+import PostCommentForm from "@/components/PostCommentForm";
+import PostComments from "@/components/PostComments";
 import { getPost } from "@/lib/actions";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import React from "react";
 
 type Props = {
@@ -9,9 +14,17 @@ type Props = {
 
 const page = async ({ params: { slug } }: Props) => {
   const post = await getPost(slug);
+  const session = await auth();
+
+  const editHref = `/posts/${slug}/edit`;
+
+  if (!post) {
+    //TODO: REDIRECT TO ERROR PAGE
+    redirect("/");
+  }
 
   return (
-    <div>
+    <div className='mx-auto p-8 max-w-3xl'>
       <h2>{post?.title}</h2>
       <h3>{post?.subTitle}</h3>
       {post?.content.map((paragraph) => (
@@ -19,6 +32,13 @@ const page = async ({ params: { slug } }: Props) => {
           {paragraph}
         </p>
       ))}
+      {session?.user?.admin && (
+        <div className='flex justify-end'>
+          <LinkButton href={editHref} linkText='수정' />
+        </div>
+      )}
+      <PostCommentForm postId={post?.id} authorId={session?.user?.id} />
+      <PostComments postId={post?.id} />
     </div>
   );
 };
