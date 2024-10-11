9836d4724fc1df93b0e9cd8226ea03afe70f20b2
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-11 11:04:23 +0900

    Update props type

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -2,14 +2,11 @@ import Link from "next/link";
 import React from "react";
 
 type Props = {
-  post: QueriedPostType;
+  post: QueriedPostType | null;
 };
 
 const PostLink = ({ post }: Props) => {
-  const recentDate =
-    post.createdAt >= post.updatedAt ? post.createdAt : post.updatedAt;
-
-  const displayDate = new Date(recentDate).toLocaleString("sv-SE", {
+  const displayDate = new Date(post?.createdAt || "").toLocaleString("sv-SE", {
     year: "numeric",
     month: "numeric",
     day: "numeric",
@@ -21,11 +18,11 @@ const PostLink = ({ post }: Props) => {
     <article>
       <div>
         <h3 className='text-xl mb-2 cursor-pointer'>
-          <Link href={`/posts/${post.id}`}>{post.title}</Link>
+          <Link href={`/posts/${post?.id}`}>{post?.title}</Link>
         </h3>
-        <h4>{post.subTitle}</h4>
+        <h4>{post?.subTitle}</h4>
         <div>
-          <span>{post.author?.username}</span> <span>{displayDate}</span>
+          <span>{post?.author?.username}</span> <span>{displayDate}</span>
         </div>
       </div>
     </article>
