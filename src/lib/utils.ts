29a27552774509d76b57dfb909f3f8606018b2ba
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-11 17:50:51 +0900

    Add formatDate

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,3 +3,13 @@ export const postContentToArray = (content: string) =>
     .toString()
     .split("\n")
     .filter((el) => el.trim());
+
+export const formatDate = (date: Date) => {
+  return new Date(date).toLocaleString("sv-SE", {
+    year: "numeric",
+    month: "numeric",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
