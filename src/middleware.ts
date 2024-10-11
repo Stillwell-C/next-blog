715eeccc5db80cf533de066002483b3568cd7bca
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-11 17:51:40 +0900

    Add export

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,9 @@
 export { auth as middleware } from "@/lib/auth";
+import NextAuth from "next-auth";
+import { authConfig } from "./lib/authConfig";
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ["/((?!api|static|.*\\..*|_next).*)"],
+};
