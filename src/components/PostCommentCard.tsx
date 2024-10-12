1f21b1c2445cd13d4fa406640c7777f1ea474a81
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-12 17:01:25 +0900

    Move image logic to ProfileImage component

--- src/components/PostCommentCard.tsx
+++ src/components/PostCommentCard.tsx
@@ -1,31 +1,17 @@
-import Image from "next/image";
-import { RxAvatar } from "react-icons/rx";
+import ProfileImage from "./ProfileImage";
 
 type Props = {
   comment: CommentType;
 };
 
 const PostCommentCard = ({ comment }: Props) => {
-  const blankAvatar = <RxAvatar size={40} />;
-  const generateAvatar = (url: string) => (
-    <Image
-      src={url}
-      width={40}
-      height={40}
-      alt={`${comment.author.username} 프로필 사진`}
-      className='rounded-full'
-    ></Image>
-  );
-
-  const displayAvatar = comment?.author?.imgUrl
-    ? generateAvatar(comment.author.imgUrl)
-    : blankAvatar;
-
   return (
     <div className='flex items-center gap-4'>
-      <div>{displayAvatar}</div>
+      <div>
+        <ProfileImage imgUrl={comment?.author?.imgUrl} />
+      </div>
       <div className='flex flex-col'>
-        <span>{comment.author.username}</span>
+        <span className='text-sm'>{comment.author.username}</span>
         <span>{comment.content}</span>
       </div>
     </div>
