3d2c98f521e1a2a588c9cada5811fe137b85c435
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-12 11:51:28 +0900

    Change props type

--- src/components/PostLink.tsx
+++ src/components/PostLink.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 
 type Props = {
-  post: QueriedPostType | null;
+  post: FindManyPostType | null;
 };
 
 const PostLink = ({ post }: Props) => {
