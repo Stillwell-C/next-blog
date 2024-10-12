80c04c2b57abdd1aa5de9e8ad34f40a3bc6a6e99
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-12 12:01:20 +0900

    Add PostResults component

--- src/components/RecentPosts.tsx
+++ src/components/RecentPosts.tsx
@@ -1,20 +1,15 @@
 import React from "react";
-import PostLink from "./PostLink";
 import { getPosts } from "@/lib/actions";
 import LinkButton from "./LinkButton";
+import PostResults from "./PostResults";
 
 const RecentPosts = async () => {
   const postData = await getPosts({ take: 5 });
 
   return (
     <div className='mx-auto max-w-3xl h-full flex flex-col items-center'>
-      <div className='flex-1 flex flex-col gap-6 items-center'>
-        <h2 className='text-3xl'>최근 글</h2>
-        <div className='flex flex-col gap-4'>
-          {postData?.posts?.map((post) => (
-            <PostLink post={post} key={post.id} />
-          ))}
-        </div>
+      <PostResults posts={postData.posts} pageTitle='최근 글' />
+      <div className='mt-6'>
         <LinkButton href='/posts' linkText='모든 글' />
       </div>
     </div>
