224e213c8e1387e5103b25974684d10545349564
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-13 18:34:48 +0900

    Remove authConfig import

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,4 @@
 export { auth as middleware } from "@/lib/auth";
-import NextAuth from "next-auth";
-import { authConfig } from "./lib/authConfig";
-
-export default NextAuth(authConfig).auth;
 
 export const config = {
   matcher: ["/((?!api|static|.*\\..*|_next).*)"],
