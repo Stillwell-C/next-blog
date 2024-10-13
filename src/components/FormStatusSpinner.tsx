eae87451e975fbb6965e38aadda1eaa46364f5cb
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-13 18:16:55 +0900

    Initial commit

---
+++ src/components/FormStatusSpinner.tsx
@@ -0,0 +1,14 @@
+import { useFormStatus } from "react-dom";
+import { ClipLoader } from "react-spinners";
+
+type Props = {
+  size?: number;
+};
+
+const FormStatusSpinner = ({ size = 35 }: Props) => {
+  const { pending } = useFormStatus();
+
+  if (pending) return <ClipLoader size={size} />;
+};
+
+export default FormStatusSpinner;
