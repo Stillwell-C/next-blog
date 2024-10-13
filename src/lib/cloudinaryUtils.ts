0c7cc5ae5d4dd549dd5098b05692aadd1956def6
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-13 11:30:21 +0900

    Add server action cloudinary upload

--- src/lib/cloudinaryUtils.ts
+++ src/lib/cloudinaryUtils.ts
@@ -1,3 +1,5 @@
+import { cloudinary } from "@/cloudinary.config";
+
 export const uploadFileToCloudinary = async (file: File | Blob) => {
   try {
     const form = new FormData();
@@ -12,6 +14,44 @@ export const uploadFileToCloudinary = async (file: File | Blob) => {
 
     return data;
   } catch (err) {
+    console.log(err);
     throw new Error();
+  }
+};
+
+export const uploadFileToCloudinaryFromAction = async (file: File | Blob) => {
+  try {
+    const fileBuffer = await file.arrayBuffer();
+    const mimeType = file.type;
+    const encoding = "base64";
+    const encodedData = Buffer.from(fileBuffer).toString("base64");
+
+    const fileUri = `data:${mimeType};${encoding},${encodedData}`;
+
+    const response = await uploadImage(fileUri);
+
+    if (response.success && response.result) {
+      return {
+        message: "Image uploaded",
+        error: false,
+        imgUrl: response.result.secure_url,
+      };
+    } else {
+      return { message: "Upload failed", error: true };
+    }
+  } catch (err) {
+    return { message: "Upload failed", error: true };
+  }
+};
+
+const uploadImage = async (fileUri: string) => {
+  try {
+    const result = await cloudinary.uploader.upload(fileUri, {
+      invalidate: true,
+      resource_type: "auto",
+    });
+    return { success: true, result };
+  } catch (err) {
+    return { success: false, err };
   }
 };
