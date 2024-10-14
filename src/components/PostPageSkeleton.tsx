2b7a9b1617b91898d8a48c78575198a668089468
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-14 18:34:19 +0900

    Set skeleton themes

--- src/components/PostPageSkeleton.tsx
+++ src/components/PostPageSkeleton.tsx
@@ -1,26 +1,28 @@
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const PostPageSkeleton = () => {
   return (
     <section className='mx-auto p-8 max-w-3xl flex-1'>
-      <h2 className='text-2xl mb-8'>
-        <Skeleton containerClassName='flex-1' />
-      </h2>
-      <div className='flex flex-col gap-4'>
-        <p>
-          <Skeleton count={10} />
-        </p>
-        <p>
-          <Skeleton count={10} />
-        </p>
-        <p>
-          <Skeleton count={10} />
-        </p>
-        <p>
-          <Skeleton count={10} />
-        </p>
-      </div>
+      <SkeletonTheme baseColor='#B7BDC2' highlightColor='#E5E7EB'>
+        <h2 className='text-2xl mb-8'>
+          <Skeleton containerClassName='flex-1' />
+        </h2>
+        <div className='flex flex-col gap-4'>
+          <p>
+            <Skeleton count={10} />
+          </p>
+          <p>
+            <Skeleton count={10} />
+          </p>
+          <p>
+            <Skeleton count={10} />
+          </p>
+          <p>
+            <Skeleton count={10} />
+          </p>
+        </div>
+      </SkeletonTheme>
     </section>
   );
 };
--- src/components/PostResultsSkeleton.tsx
+++ src/components/PostResultsSkeleton.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { SkeletonTheme } from "react-loading-skeleton";
 import PostCardSkeleton from "./PostCardSkeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -5,11 +8,13 @@ const PostResultsSkeleton = () => {
   return (
     <div className='mx-auto max-w-3xl flex flex-col gap-6 items-center'>
       <div className='w-full p-8 flex flex-col items-center gap-4'>
-        <PostCardSkeleton />
-        <PostCardSkeleton />
-        <PostCardSkeleton />
-        <PostCardSkeleton />
-        <PostCardSkeleton />
+        <SkeletonTheme baseColor='#B7BDC2' highlightColor='#E5E7EB'>
+          <PostCardSkeleton />
+          <PostCardSkeleton />
+          <PostCardSkeleton />
+          <PostCardSkeleton />
+          <PostCardSkeleton />
+        </SkeletonTheme>
       </div>
     </div>
   );
