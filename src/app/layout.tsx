8e1ddfd901c57a2c9f52e2aa82655de7f37d1a83
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-14 15:37:56 +0900

    Apply themes provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { SessionProvider } from "next-auth/react";
+import { Providers } from "./providers";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,15 +29,17 @@ export default function RootLayout({
 }>) {
   return (
     <SessionProvider>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body className={`${geistSans.variable} ${geistMono.variable} `}>
-          <div className='flex justify-center flex-1 dark:bg-gray-900 dark:text-white'>
-            <div className='min-h-screen max-w-7xl flex flex-col items-center flex-1'>
-              <Navbar />
-              <main className='flex-1 w-full'>{children}</main>
-              <Footer />
+          <Providers>
+            <div className='flex justify-center flex-1 dark:bg-gray-900 dark:text-white'>
+              <div className='min-h-screen max-w-7xl flex flex-col items-center flex-1'>
+                <Navbar />
+                <main className='flex-1 w-full'>{children}</main>
+                <Footer />
+              </div>
             </div>
-          </div>
+          </Providers>
         </body>
       </html>
     </SessionProvider>
