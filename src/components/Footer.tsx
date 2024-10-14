e9362a8473e6a02dc0104faddb18d08ba163fe66
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-14 17:49:05 +0900

    Add icon and links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
+import { FaGithub } from "react-icons/fa";
+
 const Footer = () => {
-  return <footer className='p-8'>Footer</footer>;
+  return (
+    <footer className='p-10 flex items-center gap-4'>
+      <a href='https://github.com/Stillwell-C'>Stillwell-C</a>
+      <span>|</span>
+      <a href='https://github.com/Stillwell-C'>
+        <FaGithub size={20} />
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
