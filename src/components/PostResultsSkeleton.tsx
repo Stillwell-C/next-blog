e62f2ce254f1340a6a0df21fdf0c7f52ed6abfb0
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-14 14:51:26 +0900

    Increase padding

--- src/components/PostResultsSkeleton.tsx
+++ src/components/PostResultsSkeleton.tsx
@@ -4,7 +4,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 const PostResultsSkeleton = () => {
   return (
     <div className='mx-auto max-w-3xl flex flex-col gap-6 items-center'>
-      <div className='w-full p-4 flex flex-col items-center gap-4'>
+      <div className='w-full p-8 flex flex-col items-center gap-4'>
         <PostCardSkeleton />
         <PostCardSkeleton />
         <PostCardSkeleton />
