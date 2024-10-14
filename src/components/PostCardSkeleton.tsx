2586521cbaecabded230886d1200198f819c6649
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-14 19:24:32 +0900

    Change responsive flex styles

--- src/components/PostCardSkeleton.tsx
+++ src/components/PostCardSkeleton.tsx
@@ -3,12 +3,12 @@ import Skeleton from "react-loading-skeleton";
 
 const PostCardSkeleton = () => {
   return (
-    <div className='p-6 w-full flex gap-8 shadow-md rounded-lg flex-row-reverse items-center'>
-      <div className='w-1/3 h-full'>
+    <div className='p-6 w-full flex flex-col md:flex-row gap-8 shadow-md rounded-lg items-center'>
+      <div className='md:w-1/3 h-full'>
         <Skeleton height={180} />
       </div>
 
-      <div className='flex flex-col justify-between gap-4 w-2/3'>
+      <div className='flex flex-col justify-between gap-4 md:w-2/3'>
         <h3 className='text-3xl '>
           <Skeleton />
         </h3>
