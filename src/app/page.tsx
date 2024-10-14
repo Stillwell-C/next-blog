e568e63e97d02e23f218e37560fec0e7bac55910
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-14 12:39:04 +0900

    Add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import PostResultsSkeleton from "@/components/PostResultsSkeleton";
 import RecentPosts from "@/components/RecentPosts";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className='px-8'>
-      <RecentPosts />
+      <Suspense fallback={<PostResultsSkeleton />}>
+        <RecentPosts />
+      </Suspense>
     </div>
   );
 }
