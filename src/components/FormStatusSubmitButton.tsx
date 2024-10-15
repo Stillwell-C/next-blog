719aa3ad25d73591f1afa9565571b89ae71d24e8
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-15 15:54:48 +0900

    Change background color

--- src/components/FormStatusSubmitButton.tsx
+++ src/components/FormStatusSubmitButton.tsx
@@ -25,9 +25,14 @@ const FormStatusButton = ({
     <button
       type='submit'
       disabled={pending || disable}
-      className={`disabled:cursor-not-allowed bg-black dark:bg-slate-600 white disabled:opacity-50 text-white items-center justify-center py-2 px-8 rounded min-w-28 min-h-11 ${
-        dangerColor ? "bg-red-500 dark:bg-red-500" : ""
-      }`}
+      className={`disabled:cursor-not-allowed 
+        ${
+          dangerColor
+            ? "bg-red-500 dark:bg-red-500"
+            : "bg-black dark:bg-slate-700"
+        } 
+        disabled:opacity-50 text-white items-center justify-center 
+        py-2 px-8 rounded min-w-28 min-h-11`}
     >
       {buttonContent}
     </button>
