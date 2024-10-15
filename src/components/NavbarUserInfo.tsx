8809c52732b4602174400612ad381a1185b5f6fc
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-15 09:25:36 +0900

    Add useEffect & client session to instantly change image

--- src/components/NavbarUserInfo.tsx
+++ src/components/NavbarUserInfo.tsx
@@ -1,17 +1,31 @@
 import { Session } from "next-auth";
 import ProfileImage from "./ProfileImage";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 
 type Props = { session: Session | null };
 
 const NavbarUserInfo = ({ session }: Props) => {
+  const { data: clientSession } = useSession();
+
+  const [imgUrl, setImgUrl] = useState<string | null>(
+    session?.user.imgUrl || null
+  );
+
+  useEffect(() => {
+    if (clientSession?.user.imgUrl) {
+      setImgUrl(clientSession?.user.imgUrl);
+    }
+  }, [clientSession?.user.imgUrl]);
+
   return (
     <div className='flex items-center gap-1'>
       <Link href='/profile' className='md:hidden'>
         프로필
       </Link>
       <Link href='/profile' className='hidden md:block'>
-        <ProfileImage imgUrl={session?.user.imgUrl} />
+        <ProfileImage imgUrl={imgUrl} />
       </Link>
     </div>
   );
