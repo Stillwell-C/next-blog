15fda0b6fae08858284b00f13e706c15d21d7b17
Christopher Stillwell <stillwell.c.m@gmail.com>
2024-10-16 12:25:59 +0900

    Move reply/comments logic to posts/[slug]/page.tsx

--- src/components/PostPage.tsx
+++ src/components/PostPage.tsx
@@ -2,14 +2,9 @@ import { auth } from "@/lib/auth";
 import { formatDate, resizeCloudinaryImg } from "@/lib/utils";
 import Image from "next/image";
 import { notFound } from "next/navigation";
-import React, { Suspense } from "react";
 import { RiBook2Fill } from "react-icons/ri";
 import LinkButton from "./LinkButton";
-import SubPosts from "./SubPosts";
-import PostCommentForm from "./PostCommentForm";
-import PostComments from "./PostComments";
 import { getPost } from "@/lib/actions/postActions";
-import PostCommentsListSkeleton from "./PostCommentsListSkeleton";
 
 type Props = {
   slug: string;
@@ -32,63 +27,52 @@ const PostPage = async ({ slug }: Props) => {
   }
 
   return (
-    <div className='mx-auto p-8 max-w-3xl'>
-      <section>
-        {post?.imgUrl && (
-          <div className='mx-auto mb-8 relative w-96 h-64 overflow-hidden'>
-            <Image
-              src={resizeCloudinaryImg(post.imgUrl, 500)}
-              fill
-              alt='게시물 이미지'
-              className='w-full h-full rounded-lg'
-            />
+    <section>
+      {post?.imgUrl && (
+        <div className='mx-auto mb-8 relative w-96 h-64 overflow-hidden'>
+          <Image
+            src={resizeCloudinaryImg(post.imgUrl, 500)}
+            fill
+            alt='게시물 이미지'
+            className='w-full h-full rounded-lg'
+          />
+        </div>
+      )}
+      <h2 className='text-3xl font-semibold'>{post?.title}</h2>
+      <h3 className='text-xl font-medium text-gray-700 dark:text-gray-400'>
+        {post?.subTitle}
+      </h3>
+      <div className='mb-8'>
+        <div className='flex gap-1 items-center dark:text-gray-400'>
+          <RiBook2Fill />
+          <span>읽기 시간: {readingMinutes}분</span>
+        </div>
+        <div className='flex gap-4 text-sm text-gray-500'>
+          <span>{post?.author?.username}</span>
+          <span>&#x2022;</span>
+          <span>{displayDate}</span>
+        </div>
+        {post?.editor?.username && (
+          <div className='flex gap-4 text-sm text-gray-500'>
+            <span>최신 업데이트</span>
+            <span>&#x2022;</span>
+            <span>{post?.editor?.username}</span>
+            <span>&#x2022;</span>
+            <span>{displayEditDate}</span>
           </div>
         )}
-        <h2 className='text-3xl font-semibold'>{post?.title}</h2>
-        <h3 className='text-xl font-medium text-gray-700 dark:text-gray-400'>
-          {post?.subTitle}
-        </h3>
-        <div className='mb-8'>
-          <div className='flex gap-1 items-center dark:text-gray-400'>
-            <RiBook2Fill />
-            <span>읽기 시간: {readingMinutes}분</span>
-          </div>
-          <div className='flex gap-4 text-sm text-gray-500'>
-            <span>{post?.author?.username}</span>
-            <span>&#x2022;</span>
-            <span>{displayDate}</span>
-          </div>
-          {post?.editor?.username && (
-            <div className='flex gap-4 text-sm text-gray-500'>
-              <span>최신 업데이트</span>
-              <span>&#x2022;</span>
-              <span>{post?.editor?.username}</span>
-              <span>&#x2022;</span>
-              <span>{displayEditDate}</span>
-            </div>
-          )}
-        </div>
-        {post?.content.split("\n").map((paragraph: string) => (
-          <p className='mb-4' key={paragraph.slice(0, 15)}>
-            {paragraph}
-          </p>
-        ))}
+      </div>
+      {post?.content.split("\n").map((paragraph: string) => (
+        <p className='mb-4' key={paragraph.slice(0, 15)}>
+          {paragraph}
+        </p>
+      ))}
 
-        <div className='flex gap-4 justify-end'>
-          {session?.user && (
-            <LinkButton href={replyHref} linkText='답글 쓰기' />
-          )}
-          {session?.user?.admin && (
-            <LinkButton href={editHref} linkText='수정' />
-          )}
-        </div>
-      </section>
-      <SubPosts postId={post.id} />
-      <PostCommentForm postId={post?.id} authorId={session?.user?.id} />
-      <Suspense fallback={<PostCommentsListSkeleton />}>
-        <PostComments postId={post?.id} />
-      </Suspense>
-    </div>
+      <div className='flex gap-4 justify-end'>
+        {session?.user && <LinkButton href={replyHref} linkText='답글 쓰기' />}
+        {session?.user?.admin && <LinkButton href={editHref} linkText='수정' />}
+      </div>
+    </section>
   );
 };
 
